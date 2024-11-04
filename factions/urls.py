1048033adcc7ab3bb3da09a99568e8b95e454759
oannes23 <oannes.evan@gmail.com>
2024-11-03 22:03:58 -0800

    Beginning to add Factions support

--- factions/urls.py
+++ factions/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import TimelineListView, TimelineDetailView, TimelineCreateView, TimelineUpdateView, TimelineDeleteView
+from .views import (TimelineListView, TimelineDetailView, TimelineCreateView, TimelineUpdateView, TimelineDeleteView,
+                    FactionListView, FactionDetailView, FactionCreateView, FactionUpdateView, FactionDeleteView)
 
 urlpatterns = [
+    # Timeline URLs
     path('timelines/', TimelineListView.as_view(), name='timeline_list'),
     path('timelines/<int:pk>/', TimelineDetailView.as_view(), name='timeline_detail'),
     path('timelines/create/', TimelineCreateView.as_view(), name='timeline_create'),
     path('timelines/<int:pk>/edit/', TimelineUpdateView.as_view(), name='timeline_update'),
     path('timelines/<int:pk>/delete/', TimelineDeleteView.as_view(), name='timeline_delete'),
+
+    # Faction URLs
+    path('timelines/<int:timeline_id>/factions/', FactionListView.as_view(), name='faction_list'),
+    path('timelines/<int:timeline_id>/factions/<int:pk>/', FactionDetailView.as_view(), name='faction_detail'),
+    path('timelines/<int:timeline_id>/factions/create/', FactionCreateView.as_view(), name='faction_create'),
+    path('timelines/<int:timeline_id>/factions/<int:pk>/edit/', FactionUpdateView.as_view(), name='faction_update'),
+    path('timelines/<int:timeline_id>/factions/<int:pk>/delete/', FactionDeleteView.as_view(), name='faction_delete'),
 ]
--- factions/views.py
+++ factions/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Timeline
+from .models import Timeline, Faction
 from django.urls import reverse_lazy
 
+# Timelines
 class TimelineListView(ListView):
     model = Timeline
     template_name = 'timeline/timeline_list.html'
@@ -28,3 +29,55 @@ class TimelineDeleteView(DeleteView):
     model = Timeline
     template_name = 'timeline/timeline_confirm_delete.html'
     success_url = reverse_lazy('timeline_list')
+
+# Factions
+class FactionListView(ListView):
+    model = Faction
+    template_name = 'faction/faction_list.html'
+    context_object_name = 'factions'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['timeline'] = Timeline.objects.get(id=self.kwargs['timeline_id'])
+        return context
+
+    def get_queryset(self):
+        return Faction.objects.filter(timeline_id=self.kwargs['timeline_id'])
+
+class FactionDetailView(DetailView):
+    model = Faction
+    template_name = 'faction/faction_detail.html'
+    context_object_name = 'faction'
+
+class FactionCreateView(CreateView):
+    model = Faction
+    fields = ['name', 'description', 'tier', 'goals', 'leadership', 'values', 'history']
+    template_name = 'faction/faction_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['timeline'] = Timeline.objects.get(id=self.kwargs['timeline_id'])
+        return context
+
+    def form_valid(self, form):
+        form.instance.timeline_id = self.kwargs['timeline_id']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('faction_list', kwargs={'timeline_id': self.kwargs['timeline_id']})
+
+class FactionUpdateView(UpdateView):
+    model = Faction
+    fields = ['name', 'description', 'tier', 'goals', 'leadership', 'values', 'history']
+    template_name = 'faction/faction_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('faction_list', kwargs={'timeline_id': self.object.timeline.id})
+
+class FactionDeleteView(DeleteView):
+    model = Faction
+    template_name = 'faction/faction_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('faction_list', kwargs={'timeline_id': self.object.timeline.id})
+
